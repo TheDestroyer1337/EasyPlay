1734c86c8dd21fce3f40db356c101682d10a899e
SK <kurmann.samuel@bluewin.ch>
2015-05-18 11:52:54 +0200

    Klasse Bibliothek implementiert
    Klasse Bibliothek angegfangen zu implementieren

--- EasyPlay/EasyPlay/Bibliothek.cs
+++ EasyPlay/EasyPlay/Bibliothek.cs
@@ -8,5 +8,47 @@ namespace EasyPlay
 {
     class Bibliothek
     {
+        private List<Lied> Lieder;
+        private bool Spielt;
+
+        public Bibliothek(List<Lied> lieder, bool spielt)
+        {
+            Lieder = lieder;
+            Spielt = spielt;
+        }
+
+        public void addLied(string pfad)
+        {
+            Lied Lied = null;
+
+
+
+            Lieder.Add(Lied);
+        }
+
+        public void addOrdner(string pfad)
+        {
+
+        }
+
+        public List<Lied> getAlllLieder()
+        {
+            return Lieder;
+        }
+
+        public void liedLoeschen(string pfad)
+        {
+
+        }
+
+        public Lied getSpielendesLied()
+        {
+            foreach (Lied lied in Lieder)
+            {
+                if (lied.getSpielt())
+                    return lied;
+            }
+            return null;
+        }
     }
 }
