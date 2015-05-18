73809ece4419fa74c9875d3044308878b4ea5413
TheDestroyer1337 <oliver.koechli@gmx.ch>
2015-05-18 16:12:54 +0200

    Öffnen eines Ordners Implementiert
    Öffnen eines Ordners Implementiert
    K.O

--- EasyPlay/EasyPlay/MainWindow.xaml.cs
+++ EasyPlay/EasyPlay/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -12,8 +13,9 @@ using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
 using System.Windows.Shapes;
-using System.Windows.Media;
 using System.Windows.Threading;
+using Microsoft.Win32;
+using System.Windows.Forms;
 
 namespace EasyPlay
 {
@@ -31,6 +33,11 @@ namespace EasyPlay
         private DispatcherTimer Timer;
         private TimeSpan Pause;
 
+        private enum MyType
+        {
+            Titel, Interpret, Album, Playlists, Warteliste
+        }
+
         public MainWindow()
         {
             InitializeComponent();
@@ -39,14 +46,26 @@ namespace EasyPlay
             PlaylistGrid.Visibility = Visibility.Hidden;
             InterpretenGrid.Visibility = Visibility.Hidden;
             AlbenGrid.Visibility = Visibility.Hidden;
-            
-            Player = new MediaPlayer();
+
+            string saveFile = AppDomain.CurrentDomain.BaseDirectory + "easyplay.txt";
+            if (!File.Exists(saveFile))
+            {
+                FolderBrowserDialog ofd = new FolderBrowserDialog();
+                ofd.Description = "Ordner öffnen";
+                ofd.ShowNewFolderButton = false;
+                bool result = false;
+                result = Convert.ToBoolean(ofd.ShowDialog());
+                Biblio = new Bibliothek(ofd.SelectedPath);
+                this.displayData(MyType.Titel);
+            }
+
+        Player = new MediaPlayer();
             
         }
 
         private void TitelButton_Clicked(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Titel button is clicked.");
+            System.Windows.MessageBox.Show("Titel button is clicked.");
             TitelGrid.Visibility = Visibility.Visible;
             PlaylistGrid.Visibility = Visibility.Hidden;
             AlbenGrid.Visibility = Visibility.Hidden;
@@ -55,7 +74,7 @@ namespace EasyPlay
 
         private void PlaylistsButton_Clicked(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Playlists button is clicked.");
+            System.Windows.MessageBox.Show("Playlists button is clicked.");
             TitelGrid.Visibility = Visibility.Hidden;
             PlaylistGrid.Visibility = Visibility.Visible;
             AlbenGrid.Visibility = Visibility.Hidden;
@@ -64,7 +83,7 @@ namespace EasyPlay
 
         private void AlbenButton_Clicked(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Alben button is clicked.");
+            System.Windows.MessageBox.Show("Alben button is clicked.");
             TitelGrid.Visibility = Visibility.Hidden;
             PlaylistGrid.Visibility = Visibility.Hidden;
             AlbenGrid.Visibility = Visibility.Visible;
@@ -73,7 +92,7 @@ namespace EasyPlay
 
         private void InterpretenButton_Clicked(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Interpreten button is clicked.");
+            System.Windows.MessageBox.Show("Interpreten button is clicked.");
             TitelGrid.Visibility = Visibility.Hidden;
             PlaylistGrid.Visibility = Visibility.Hidden;
             AlbenGrid.Visibility = Visibility.Hidden;
@@ -82,7 +101,7 @@ namespace EasyPlay
 
         private void WartelisteButton_Clicked(object sender, RoutedEventArgs e)
         {
-            MessageBox.Show("Warteliste button is clicked.");
+            System.Windows.MessageBox.Show("Warteliste button is clicked.");
             TitelGrid.Visibility = Visibility.Visible;
             PlaylistGrid.Visibility = Visibility.Hidden;
             AlbenGrid.Visibility = Visibility.Hidden;
@@ -97,6 +116,23 @@ namespace EasyPlay
         private void BtnPlayPause_Click(object sender, RoutedEventArgs e)
         {
        
+        }
+
+        private void displayData(MyType type)
+        {
+            switch (type)
+            {
+                case MyType.Titel:
+                    break;
+                case MyType.Album:
+                    break;
+                case MyType.Interpret:
+                    break;
+                case MyType.Playlists:
+                    break;
+                case MyType.Warteliste:
+                    break;
+            }
         }
     }
 }
